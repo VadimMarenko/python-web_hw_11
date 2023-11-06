610eed8a747780b9ef55e51907694300595062fc
VadimMarenko <130446385+VadimMarenko@users.noreply.github.com>
2023-11-06 20:03:58 +0200

    update
    db.py   echo=True

--- src/database/db.py
+++ src/database/db.py
@@ -3,6 +3,8 @@ import pathlib
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+from fastapi import HTTPException, status
 
 
 file_config = pathlib.Path(__file__).parent.parent.joinpath("conf/config.ini")
@@ -16,7 +18,7 @@ domain = config.get("DB", "DOMAIN")
 port = config.get("DB", "PORT")
 
 url = f"postgresql+psycopg2://{username}:{password}@{domain}:{port}/{database_name}"
-engine = create_engine(url, echo=False, pool_size=5)
+engine = create_engine(url, echo=True, pool_size=5)
 
 DBSession = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
@@ -26,5 +28,8 @@ def get_db():
     db = DBSession()
     try:
         yield db
+    except SQLAlchemyError as err:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err))
     finally:
         db.close()
