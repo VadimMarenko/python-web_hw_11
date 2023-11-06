bc537d0a226596aa88dd86e4dfeb4b814929d5a6
VadimMarenko <130446385+VadimMarenko@users.noreply.github.com>
2023-11-06 21:40:13 +0200

    Update schemas.py

--- src/schemas.py
+++ src/schemas.py
@@ -1,15 +1,25 @@
+from datetime import date
+
 from pydantic import BaseModel, EmailStr, Field
 
 
 class UserModel(BaseModel):
-    first_name: str = Field("Bill", min_length=3, max_length=12)
-    email: EmailStr
-    description: str
+    first_name: str = Field("Bill")
+    last_name: str = Field("Jhonson")
+    email: EmailStr = Field("user@example.com")
+    phone_number: str = Field("+380 99 99 99 999")
+    born_date: date = date(2000, 1, 1)
+    description: str = Field("Customer")
 
 
 class ResponseUser(BaseModel):
-    id: int = 1
+    id: int
+    first_name: str
+    last_name: str
     email: EmailStr
+    phone_number: str
+    born_date: date
+    description: str
 
     class Config:
         orm_mode = True
