5f4d220c66ca69d8f679dd0d99ee1a06094fd286
VadimMarenko <130446385+VadimMarenko@users.noreply.github.com>
2023-11-08 20:24:55 +0200

    Update schemas.py

--- src/schemas.py
+++ src/schemas.py
@@ -4,25 +4,25 @@ from pydantic import BaseModel, EmailStr, Field
 
 
 class UserModel(BaseModel):
-    first_name: str = Field(max_length=25)
-    last_name: str = Field(max_length=30)
-    email: EmailStr
-    phone_number: str = Field(max_length=25)
-    born_date: date
-    description: str = Field(max_length=250)
+    first_name: str = Field(default="", max_length=25)
+    last_name: str = Field(default="", max_length=30)
+    email: EmailStr | None
+    phone_number: str = Field(default="", max_length=25)
+    born_date: date | None = None
+    description: str = Field(default="", max_length=250)
 
 
 class ResponseUser(BaseModel):
     id: int
-    first_name: str
-    last_name: str
-    email: EmailStr
-    phone_number: str
-    born_date: date
-    description: str | None = None
+    first_name: str | None
+    last_name: str | None
+    email: EmailStr | None
+    phone_number: str | None
+    born_date: date | None
+    description: str | None
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class UserEmailModel(BaseModel):
